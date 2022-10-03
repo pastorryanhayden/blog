999b0b271f088c38cfe0eaa86a48d43cd9df762c
Ryan Hayden <ryanhayden@Ryans-Mac-mini-2.local>
2022-10-03 09:50:52 -0500

    added comments from talkyard

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -44,4 +44,12 @@
             @endif
         </div>
     </nav>
+    <script>talkyardServerUrl='https://site-1f97zwdm3a.talkyard.net';</script>
+<script async defer src="https://c1.ty-cdn.net/-/talkyard-comments.min.js"></script>
+<!-- You can specify a per page discussion id on the next line, if your URLs might change. -->
+<div class="talkyard-comments" data-discussion-id="" style="margin-top: 45px;">
+<noscript>Please enable Javascript to view comments.</noscript>
+<p style="margin-top: 25px; opacity: 0.9; font-size: 96%">Comments powered by
+<a href="https://www.talkyard.io">Talkyard</a>.</p>
+</div>
 @endsection
